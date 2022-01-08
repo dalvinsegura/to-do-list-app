079204776f0a31912b4389b4b9cf72b2354e74a0
Dalvin <byjacson18@gmail.com>
2022-01-08 15:37:16 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from "react";
+import './components/styles/normalize.css';
+import TaskContainer from "./components/TaskContainer";
+import "./components/styles/app.scss"
+function App() {
+  return (
+    <div className="App">
+      <TaskContainer/>
+    </div>
+  );
+}
+
+export default App;
